f025aa699e2e57390505c8262e038bea16b9c486
S. M. Zuhair Zaki <zuhairzaki500@gmail.com>
2021-06-01 21:54:43 +0600

    Add files via upload

---
+++ 1705023.y
@@ -0,0 +1,251 @@
+%{
+#include <bits/stdc++.h>
+#include "SymbolTable.h"
+#define YYSTYPE SymbolInfo*
+
+using namespace std;
+
+int yyparse(void);
+int yylex(void);
+
+FILE *fp;
+extern FILE *yyin;
+extern int yylineno;
+
+extern int line_no;
+extern int error_count;
+
+int n = 30;
+ScopeTable* globalScope = new ScopeTable(n);
+SymbolTable* symTab = new SymbolTable(n,globalScope);
+
+ofstream errorFile("error.txt");
+ofstream logFile("log.txt");
+
+void yyerror(char *str)
+{
+	cout<<"Syntax error"<<endl;
+}
+
+SymbolInfo* insertItem(SymbolInfo* head,SymbolInfo* s)
+{
+	SymbolInfo* y = head;
+	SymbolInfo* prev_y = NULL;
+
+	while(y!=NULL){
+		prev_y = y;
+		y = y->nextInfoObj;
+	}
+
+	if(prev_y==NULL)
+		head = s;
+	else prev_y->nextInfoObj = s;
+
+	return head;
+}
+
+
+%}
+
+%token IF ID LPAREN RPAREN SEMICOLON COMMA LCURL RCURL INT FLOAT VOID
+%token LTHIRD RTHIRD CONST_INT CONST_FLOAT FOR WHILE RETURN PRINTLN
+%token ASSIGNOP LOGICOP RELOP ADDOP MULOP INCOP DECOP NOT
+
+%nonassoc THEN
+%nonassoc ELSE
+
+%%
+
+start : program 
+			{
+				symTab->printAllScope(logFile);
+
+				logFile<<"Total lines : "<<yylineno<<endl;
+				logFile<<"Total error : "<<error_count<<endl;
+			}
+	;
+
+program : program unit 
+	| unit
+	;
+	
+unit : var_declaration
+     | func_declaration
+     | func_definition
+     ;
+     
+func_declaration : type_specifier ID LPAREN parameter_list RPAREN SEMICOLON
+		| type_specifier ID LPAREN RPAREN SEMICOLON
+		;
+		 
+func_definition : type_specifier ID LPAREN parameter_list RPAREN compound_statement
+		| type_specifier ID LPAREN RPAREN compound_statement
+ 		;				
+
+
+parameter_list  : parameter_list COMMA type_specifier ID
+		| parameter_list COMMA type_specifier
+ 		| type_specifier ID
+		| type_specifier
+ 		;
+
+ 		
+compound_statement : LCURL statements RCURL
+ 		    | LCURL RCURL
+ 		    ;
+ 		    
+var_declaration : type_specifier declaration_list SEMICOLON 
+					{	
+						logFile<<"Line "<<yylineno<<": var_declaration : type_specifier declaration_list SEMICOLON"<<endl;
+
+						string varType = $1->getName();
+
+						SymbolInfo* head = $2;
+
+						while(head!=NULL){
+							SymbolInfo* item = new SymbolInfo(head->getName(),head->getType());
+							item->setDataType(varType);
+
+							if(!symTab->Insert(item)){
+								error_count++;
+
+								errorFile<<"Error at line "<<yylineno<<": Multiple declaration of "<<head->getName()<<endl;
+							}
+							head = head->nextInfoObj;
+						}
+
+						delete $1;
+					}
+ 		 ;
+ 		 
+type_specifier	: INT {
+							logFile<<"Line "<<yylineno<<": type_specifier : INT"<<endl;
+							$$ = new SymbolInfo("INT","KEYWORD");
+					}
+ 		| FLOAT {
+			 		logFile<<"Line "<<yylineno<<": type_specifier : FLOAT"<<endl;
+					$$ = new SymbolInfo("FLOAT","KEYWORD");
+		 		}
+ 		| VOID {
+			 		logFile<<"Line "<<yylineno<<": type_specifier : VOID"<<endl;
+					$$ = new SymbolInfo("VOID","KEYWORD");
+		 	}
+ 		;
+ 		
+declaration_list : declaration_list COMMA ID 
+					{	
+						logFile<<"Line "<<yylineno<<": declaration_list : declaration_list COMMA ID "<<endl;
+						$$ = insertItem($1,$3);
+					}
+ 		  | declaration_list COMMA ID LTHIRD CONST_INT RTHIRD 
+					{	
+						logFile<<"Line "<<yylineno<<": declaration_list : declaration_list COMMA ID LTHIRD CONST_INT RTHIRD "<<endl;
+						$$ = insertItem($1,$3);
+					}
+ 		  | ID {	
+			   		logFile<<"Line "<<yylineno<<": declaration_list : ID "<<endl;
+			   		string varType = $1->getDataType();
+
+					if(varType=="NO_TYPE"){
+						$$ = $1;
+					}
+					else{
+						$$ = new SymbolInfo($1->getName(),$1->getType());
+					}
+		   	   }
+ 		  | ID LTHIRD CONST_INT RTHIRD 
+				{
+					logFile<<"Line "<<yylineno<<": declaration_list : ID LTHIRD CONST_INT RTHIRD "<<endl;
+
+					string varType = $1->getDataType();
+
+					if(varType=="NO_TYPE"){
+						$$ = $1;
+					}
+					else{
+						$$ = new SymbolInfo($1->getName(),$1->getType());
+					}
+				}
+ 		  ;
+ 		  
+statements : statement
+	   | statements statement
+	   ;
+	   
+statement : var_declaration
+	  | expression_statement
+	  | compound_statement
+	  | FOR LPAREN expression_statement expression_statement expression RPAREN statement
+	  | IF LPAREN expression RPAREN statement %prec THEN
+	  | IF LPAREN expression RPAREN statement ELSE statement
+	  | WHILE LPAREN expression RPAREN statement
+	  | PRINTLN LPAREN ID RPAREN SEMICOLON
+	  | RETURN expression SEMICOLON
+	  ;
+	  
+expression_statement 	: SEMICOLON			
+			| expression SEMICOLON 
+			;
+	  
+variable : ID 		
+	 | ID LTHIRD expression RTHIRD 
+	 ;
+	 
+ expression : logic_expression	
+	   | variable ASSIGNOP logic_expression 	
+	   ;
+			
+logic_expression : rel_expression 	
+		 | rel_expression LOGICOP rel_expression 	
+		 ;
+			
+rel_expression	: simple_expression 
+		| simple_expression RELOP simple_expression	
+		;
+				
+simple_expression : term 
+		  | simple_expression ADDOP term 
+		  ;
+					
+term :	unary_expression
+     |  term MULOP unary_expression
+     ;
+
+unary_expression : ADDOP unary_expression  
+		 | NOT unary_expression 
+		 | factor 
+		 ;
+	
+factor	: variable 
+	| ID LPAREN argument_list RPAREN
+	| LPAREN expression RPAREN
+	| CONST_INT 
+	| CONST_FLOAT
+	| variable INCOP 
+	| variable DECOP
+	;
+	
+argument_list : arguments
+			  |
+			  ;
+	
+arguments : arguments COMMA logic_expression
+	      | logic_expression
+	      ;
+ 
+
+%%
+int main(int argc,char *argv[])
+{
+	if((fp=fopen(argv[1],"r"))==NULL)
+	{
+		printf("Cannot Open Input File.\n");
+		exit(1);
+	}
+	
+
+	yyin=fp;
+	yyparse();
+	
+	return 0;
+}
---
+++ SymbolTable.h
@@ -0,0 +1,323 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SymbolInfo
+{
+    string Name, Type;
+    string dataType;
+
+public:
+    SymbolInfo* nextInfoObj;
+
+    SymbolInfo() {  nextInfoObj = NULL; dataType="NO_TYPE"; }
+    SymbolInfo(string name,string type){
+        this->Name = name;
+        this->Type = type;
+        nextInfoObj = NULL;
+        dataType = "NO_TYPE";
+    }
+
+    void setName(string new_name){
+        Name = new_name;
+    }
+    void setType(string new_type){
+        Type = new_type;
+    }
+    void setDataType(string new_type){
+        dataType = new_type;
+    }
+    
+    string getName()
+    {
+        return Name;
+    }
+
+    string getType()
+    {
+        return Type;
+    }
+
+    string getDataType()
+    {
+        return dataType;
+    }
+};
+
+class ScopeTable
+{
+    int Hsize;
+    int id;
+    SymbolInfo** table;
+
+public:
+    ScopeTable* parentScope;
+
+    ScopeTable()
+    {
+        id = 0;
+        Hsize = 0;
+        table = NULL;
+        parentScope = NULL;
+    }
+
+    ScopeTable(int n)
+    {
+        id = 0;
+        Hsize = n;
+        table = new SymbolInfo*[Hsize];
+        for(int i=0;i<Hsize;i++)
+            table[i]= NULL;
+        parentScope = NULL;
+    }
+
+    void setID(int new_id)
+    {
+        id = new_id;
+    }
+
+    int getID()
+    {
+        return id;
+    }
+
+    string constructID()
+    {
+        if(parentScope == NULL)
+            return to_string(id);
+        return parentScope->constructID() + "."  + to_string(id);
+    }
+
+    void setTableSize(int new_size)
+    {
+        Hsize = new_size;
+    }
+
+    int getTableSize()
+    {
+        return Hsize;
+    }
+
+    int hash_func(string key)
+    {
+        int hash_val = 0;
+        for(int i=0;i<key.size();i++)
+            hash_val += key[i];
+        hash_val %= Hsize;
+
+        return hash_val;
+    }
+
+    bool Insert(SymbolInfo* x)
+    {
+        int idx = hash_func(x->getName());
+        int pos = 0;
+
+        SymbolInfo* y = table[idx];
+        SymbolInfo* prev_y = NULL;
+        while(y!=NULL){
+            if(x->getName() == y->getName())
+                return false;
+            prev_y = y;
+            y = y->nextInfoObj;
+            pos++;
+        }
+
+        if(prev_y == NULL)
+            table[idx] = x;
+        else
+            prev_y->nextInfoObj = x;
+        return true;
+    }
+
+    SymbolInfo* Lookup(string symbol)
+    {
+        int idx = hash_func(symbol);
+        int pos = 0;
+
+        SymbolInfo* x = table[idx];
+        while(x!=NULL){
+            if(x->getName() == symbol){
+                return x;
+            }
+            x = x->nextInfoObj;
+            pos++;
+        }
+        return NULL;
+    }
+
+    bool Delete(string symbol)
+    {
+        int idx = hash_func(symbol);
+        int pos = 0;
+
+        SymbolInfo* x = table[idx];
+        SymbolInfo* prev_x = NULL;
+        while(x!=NULL){
+            if(x->getName() == symbol){
+                if(prev_x==NULL)
+                    table[idx] = x->nextInfoObj;
+                else prev_x->nextInfoObj = x->nextInfoObj;
+
+                delete x;
+
+                return true;
+            }
+            prev_x = x;
+            x = x->nextInfoObj;
+            pos++;
+        }
+        return false;
+    }
+
+    void printTable(ofstream& ofs)
+    {
+        ofs<<"ScopeTable # "<<constructID()<<endl;
+        for(int i=0;i<Hsize;i++){
+            SymbolInfo* x = table[i];
+            
+            if(x!=NULL){
+            ofs<<" "<<i<<" --> ";
+            while(x!=NULL){
+                ofs<<"< ";
+                ofs<<x->getName();
+                ofs<<" : ";
+                ofs<<x->getType();
+                ofs<<"> ";
+
+                x = x->nextInfoObj;
+            }
+            ofs<<endl;
+            }
+            
+        }
+    }
+
+    ~ScopeTable()
+    {
+        for(int i=0;i<Hsize;i++){
+            SymbolInfo* x = table[i];
+            while(x!=NULL){
+                SymbolInfo* y = x;
+                x = x->nextInfoObj;
+                delete y;
+            }
+        }
+
+        delete[] table;
+    }
+};
+
+class SymbolTable
+{
+    int bucketSize;
+    ScopeTable* currScope;
+    stack<int> lvlSerial;
+
+public:
+
+    SymbolTable()
+    {
+        bucketSize = 0;
+        currScope = NULL;
+    }
+
+    SymbolTable(int n,ScopeTable* gScope)
+    {
+        bucketSize = n;
+        currScope = gScope;
+        currScope->setID(1);
+    }
+
+    void setBucketSize(int new_size)
+    {
+        bucketSize = new_size;
+    }
+
+    int getBucketSize()
+    {
+        return bucketSize;
+    }
+
+    void enterScope()
+    {
+        int scope_id;
+        if(lvlSerial.empty()){
+            scope_id = 1;
+        }
+        else{
+            scope_id = lvlSerial.top() + 1;
+            lvlSerial.pop();
+        }
+
+        ScopeTable* new_scope = new ScopeTable(bucketSize);
+        new_scope->setID(scope_id);
+        new_scope->parentScope = currScope;
+        currScope = new_scope;
+    }
+
+    void exitScope()
+    {
+        if(currScope == NULL)
+            return;
+
+        ScopeTable* exited_scope = currScope;
+        currScope = exited_scope->parentScope;
+
+        if(!lvlSerial.empty())
+            lvlSerial.pop();
+        lvlSerial.push(exited_scope->getID());
+
+        delete exited_scope;
+    }
+
+    bool Insert(SymbolInfo* new_entry)
+    {
+        if(currScope == NULL)
+            return false;
+
+        return currScope->Insert(new_entry);
+    }
+
+    bool Remove(string symbol)
+    {
+        if(currScope == NULL)
+            return false;
+        return currScope->Delete(symbol);
+    }
+
+    SymbolInfo* Lookup(string symbol)
+    {
+        if(currScope == NULL)
+            return NULL;
+
+        ScopeTable* temp_scope = currScope;
+        while(temp_scope!=NULL){
+            SymbolInfo* x = temp_scope->Lookup(symbol);
+            if(x!=NULL)
+                return x;
+            temp_scope = temp_scope->parentScope;
+        }
+        return NULL;
+    }
+
+    void printCurrScope(ofstream& ofs)
+    {
+        if(currScope == NULL)
+            return;
+        currScope->printTable(ofs);
+    }
+
+    void printAllScope(ofstream& ofs)
+    {
+        if(currScope == NULL)
+            return;
+        ScopeTable* temp_scope = currScope;
+        while(temp_scope!=NULL){
+            temp_scope->printTable(ofs);
+            temp_scope = temp_scope->parentScope;
+            ofs<<endl;
+        }
+    }
+};
+
