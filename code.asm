45f5bae8bb28bc47f29cca261a7232037115c6f6
S. M. Zuhair Zaki <zuhairzaki500@gmail.com>
2021-07-01 22:19:21 +0600

    Add files via upload

--- code.asm
+++ code.asm
@@ -10,9 +10,51 @@ t0 DW ?
 t1 DW ?
 t2 DW ?
 t3 DW ?
-t4 DW ?
+t4 DW ? 
 
-.CODE
+NEWLINE DB 13, 10, '$'
+
+.CODE   
+
+PRINT_NUMBER PROC 
+    
+    PUSH BP
+    MOV BP,SP
+    
+    XOR CX, CX
+    MOV BX, 10 
+           
+        MOV AX, WORD PTR[BP+4]
+        CMP AX, 0
+        JGE GET_DIGITS  
+        SUB SP,2
+        MOV WORD PTR[BP-2],AX
+        MOV AH, 2
+        MOV DL, '-'
+        INT 21H 
+        MOV AX, WORD PTR[BP-2]
+        ADD SP,2
+        NEG AX
+    GET_DIGITS:
+        XOR DX, DX
+        DIV BX
+        PUSH DX
+        INC CX
+        CMP AX, 0
+        JNE GET_DIGITS
+        MOV AH, 2
+    PRINT_DIGITS:
+        POP DX
+        ADD DL, 30H
+        INT 21H
+        LOOP PRINT_DIGITS
+        MOV AH, 9
+        LEA DX, NEWLINE 
+        INT 21H
+        POP BP
+        RET 2    
+    
+PRINT_NUMBER ENDP
 
 main PROC
 MOV AX,@DATA
@@ -60,21 +102,24 @@ L3:
 ; b++
 
 ADD WORD PTR[BP-4], 1
-MOV t4, WORD PTR[BP-4]
+MOV AX, WORD PTR[BP-4]
+MOV t4, AX
 
 L2:
 ; a--
 
-SUB WORD PTR[BP-2], 1
-MOV t3, WORD PTR[BP-2]
+SUB WORD PTR[BP-2], 1  
+MOV AX, WORD PTR[BP-2]
+MOV t3, AX
 
 CMP t3, 0
 JNE L3
 
-; i++
+; i++ 
 
-ADD WORD PTR[BP-8], 1
-MOV t2, WORD PTR[BP-8]
+ADD WORD PTR[BP-8], 1 
+MOV AX, WORD PTR[BP-8]
+MOV t2, AX
 
 L4:
 ; i<4
@@ -94,7 +139,23 @@ MOV t1, 1
 L1:
 
 CMP t1, 0
-JNE L5
+JNE L5   
+
+
+SUB SP,8
+PUSH WORD PTR[BP-2] 
+CALL PRINT_NUMBER
+MOV SP,BP   
+
+SUB SP,8
+PUSH WORD PTR[BP-6] 
+CALL PRINT_NUMBER
+MOV SP,BP  
+
+SUB SP,8
+PUSH WORD PTR[BP-8] 
+CALL PRINT_NUMBER
+MOV SP,BP
 
 END_main:
 POP BP
