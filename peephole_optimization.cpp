a96129c1b4a34eaa2525aa68906adbcdc0da8300
S. M. Zuhair Zaki <zuhairzaki500@gmail.com>
2021-07-04 02:39:55 +0600

    Add files via upload

---
+++ peephole_optimization.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+ofstream ofs("optimized_Code.asm");
+
+void optimizeCode(ifstream& ifs)
+{
+    string line;
+    string instruction,reg1,reg2;
+
+
+    while(getline(ifs,line)){
+        string tmp,r1,r2;
+
+        if(line.empty()){
+            ofs<<line<<endl;
+            continue;
+        }
+
+        stringstream ss(line);
+        ss>>instruction;
+
+        if(instruction[0]==';'){
+            ofs<<line<<endl;
+            continue;
+        }
+
+        if(instruction == "MOV"){
+            getline(ss,tmp,',');
+            stringstream(tmp)>>r1;
+            getline(ss,tmp);
+            stringstream(tmp)>>r2;
+
+            if(!((r1==reg1&&r2==reg2)||(r1==reg2&&r2==reg1))){
+                reg1=r1;
+                reg2=r2;
+
+                ofs<<line<<endl;
+            }
+        }
+        else{
+            reg1 = "";
+            reg2 = "";
+
+            ofs<<line<<endl;
+        }
+    }
+}
+
+int main()
+{
+    ifstream ifs("code.asm");
+
+    optimizeCode(ifs);
+
+    ifs.close();
+    ofs.close();
+    return 0;
+}
